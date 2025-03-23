82f4e336a55ed0a47c5bf613829be7589679172f
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2025-03-23 11:18:22 +0100

    added animation to info section

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -2,12 +2,16 @@ import Heading from '../Heading/Heading'
 import InfoCard from '../InfoCard/InfoCard'
 import { infoCards } from '../../utils/infoCards';
 import './info.css'
+import useShowSectionAnimation from '../../hooks/useShowSectionAnimation/useShowSectionAnimation';
 
 const Info = () => {
+  const sectionId = 'info-section';
+  const isVisible = useShowSectionAnimation({sectionId});
+
   return (
     <section id='info'>
         <Heading title='Info'/>
-        <div className='info-section'>
+        <div id='info-section' className={`info-section ${isVisible ? 'visible' : ''}`}>
             {
                 infoCards.map(card => (
                     <InfoCard key={card.title}  icon={card.icon} title={card.title}/>
