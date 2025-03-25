ced0f2c8d6dab5cb8fc2d348dda1da90135fe37e
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2025-03-25 11:16:51 +0100

    added carousel to project

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,5 +1,6 @@
 import useShowSectionAnimation from '../../hooks/useShowSectionAnimation/useShowSectionAnimation';
 import { galleryImages } from '../../utils/gallery'
+import Carousel from '../Carousel/Carousel';
 import Heading from '../Heading/Heading'
 import './gallery.css'
 
@@ -12,11 +13,21 @@ const Gallery = () => {
     <section id='gallery'>
       <Heading title="Galerija"/>
       <div id='gallery-section' className={`gallery-section ${isVisble ? 'visible' : ''}`}>
-        {
+        {/* {
           galleryImages.map(img => (
               <img src={img} alt='img' key={img}/>
           ))
-        }
+        } */}
+        <div style={{ height: '600px', position: 'relative' }}>
+            <Carousel
+              baseWidth={300}
+              autoplay={true}
+              autoplayDelay={3000}
+              pauseOnHover={true}
+              loop={true}
+              round={false}
+            />
+        </div>
       </div>
     </section>
   )
