8203dab5f8a256dee47e8cc2ea63b86ebf6427a5
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2025-03-25 06:19:49 +0100

    added animation to gallery section

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,12 +1,17 @@
+import useShowSectionAnimation from '../../hooks/useShowSectionAnimation/useShowSectionAnimation';
 import { galleryImages } from '../../utils/gallery'
 import Heading from '../Heading/Heading'
 import './gallery.css'
 
 const Gallery = () => {
+
+  const sectionId = 'gallery-section';
+  const isVisble = useShowSectionAnimation({sectionId});
+
   return (
     <section id='gallery'>
       <Heading title="Galerija"/>
-      <div className='gallery'>
+      <div id='gallery-section' className={`gallery-section ${isVisble ? 'visible' : ''}`}>
         {
           galleryImages.map(img => (
               <img src={img} alt='img' key={img}/>
